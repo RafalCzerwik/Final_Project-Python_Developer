c8516af464c53c1343aaed9dab7042f6cc7ff601
Rafal Michal Czerwik <rafal.czerwik@gmail.com>
2024-03-08 14:49:19 +0100

    Models and comments

--- final_project/settings.py
+++ final_project/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'sell_it_app.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- sell_it_app/models.py
+++ sell_it_app/models.py
@@ -6,6 +6,14 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """
+    This User model inherits from Django's AbstractUser.
+    The custom fields added include:
+    gender: Can be Male (M), Female (F), or Other (O).
+    phone_number: Stores the phone number of the user. Phone number must have 9 digits.
+    date_of_birth: Stores the user's date of birth.
+    """
+
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
@@ -18,10 +26,21 @@ class User(AbstractUser):
         null=True,
         validators=[RegexValidator(regex='^[0-9]{9}$',
                                    message='Phone number must have 9 digits!')])
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(null=True)
 
 
 class Address(models.Model):
+    """
+    The Address model stores the addresses of Users.
+    The fields include:
+    user_id: Foreign key relationship to a User. This field identifies the User who is associated with the address.
+    street_name: This is the primary street name of the address.
+    street_name_secondary: This is the secondary street name of the address, if any.
+    city: This is the city of the address.
+    postal_code: Postal code corresponding to the address.
+    country: The country where the address is located.
+    """
+
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     street_name = models.CharField(max_length=255)
     street_name_secondary = models.CharField(max_length=255)
@@ -31,6 +50,12 @@ class Address(models.Model):
 
 
 class Category(models.Model):
+    """
+    The Category model is used to categorize listings into various categories.
+    name: The name of the category. Can be Market, Real estate, Boat, Motorcycle, Car, Work.
+    description: A brief description about the category.
+    """
+
     CATEGORY_CHOICES = (
         ('Market', 'Market'),
         ('Real estate', 'Real estate'),
@@ -45,6 +70,16 @@ class Category(models.Model):
 
 
 class Listings(models.Model):
+    """
+        The Listings model holds information about a certain listing.
+        user_id: A foreign key relation to the User model. Identifies the user who listed the item.
+        category_id: Foreign key relation to the Category model. Identifies the category of the listing.
+        condition: Can be 'New' or 'Used'.
+        offer_type: The type of the listing, can be 'Sell', 'Buy' or 'Free'.
+        promotion: State of the listing, can be 'Promoted' or 'Not Promoted'.
+        All other fields hold information about the particular listing.
+        """
+
     CONDITION_CHOICES = (
         ('New', 'New'),
         ('Used', 'Used'),
@@ -69,27 +104,49 @@ class Listings(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=2000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    pictures = models.ForeignKey(Picture, on_delete=models.CASCADE)
     add_date = models.DateField(auto_now_add=True)
 
 
 class Messages(models.Model):
-    from_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    """
+    The Messages model contains messages between users.
+    from_user: A foreign key relation to the User model. Identifies the sender of the message.
+    to_user: A foreign key relation to the User model. Identifies the receiver of the message.
+    title: The title of the message.
+    message: The content of the message.
+    date_sent: The date when the message was sent.
+    """
+
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_messages')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_messages')
     title = models.CharField(max_length=60)
     message = models.TextField(max_length=1000)
     date_sent = models.DateField(auto_now_add=True)
 
 
 class Picture(models.Model):
+    """
+        The Picture model is used to store images of each listing.
+        user_id: A foreign key relation to the User model. Identifies the user who uploaded the image.
+        listing_id: A foreign key relation to the Listings model. Identifies the listing the picture is attached to.
+        name: The name of the image.
+        image: The actual image file.
+        """
+
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     listing_id = models.ForeignKey(Listings, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20, blank=True, null=True)
-    image = models.ImageField(upload_to='/static/images/user-images/',
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='static/images/user-images/',
                               validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
 
 
 class Avatars(models.Model):
+    """
+        The Avatars model stores the avatar pictures for each user.
+        user_id: A foreign key relation to the User model. Identifies the user to whom the avatar belongs.
+        avatar: The actual avatar image file.
+        """
+
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='/static/images/avatars/',
+    avatar = models.ImageField(upload_to='static/images/avatars/',
                                validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
