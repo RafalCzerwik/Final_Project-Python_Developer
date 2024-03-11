09fa5d8ba3a6dab9791b400329d0d9b00fef7913
Rafal Michal Czerwik <rafal.czerwik@gmail.com>
2024-03-12 00:14:28 +0100

    admin.py updated

--- sell_it_app/admin.py
+++ sell_it_app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from sell_it_app.models import User
+from sell_it_app.models import User, Category, Address, Listings, Messages, Picture, Avatars, Newsletter
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Address)
+admin.site.register(Listings)
+admin.site.register(Messages)
+admin.site.register(Picture)
+admin.site.register(Avatars)
+admin.site.register(Newsletter)
